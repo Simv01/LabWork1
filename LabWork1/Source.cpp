a332e78ac3eec1082c5b339b3c692decce38dd03
Боднар Михайло Володимирович <mihajlobodnar8@gmail.com>
2022-09-26 18:35:06 +0300

    LabWork

---
+++ LabWork1/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
+
+void main()
+{
+	string input;
+	cin >> input;
+    cout << "Hello World!\n" + input;
+	_getch();
+
+}
